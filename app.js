2278d0bcb8e4c741497ba0faa96057be40648b2f
janetquispeu@gmail.com <janetquispeu@gmail.com>
2016-09-19 15:57:41 -0500

    subiendo formulario

---
+++ app.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function() {
+	var btn=document.getElementById("btn");	
+	function newtexto(text){
+		var elemento=document.createElement("div");
+		var conten=document.getElementById("conten");
+		elemento.innerText=text;
+		if(!conten.childNodes[0]){
+			conten.appendChild(elemento);
+		}else{
+			conten.insertBefore(elemento,conten.childNodes[0]);
+		}
+	}
+	btn.addEventListener("click",function(){
+		var text=document.getElementById("text").value;
+		newtexto(text);
+	});
+});
+
