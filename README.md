39acaa9bb620ecb93cccf6e8710304f286bd686b
janetquispeu@gmail.com <janetquispeu@gmail.com>
2016-09-20 15:17:01 -0500

    parte 4

--- README.md
+++ README.md
@@ -1,18 +1,2 @@
-# twitter
-v0.0.1
-En este ejercicio insertaremos un texto y al presionar en el botón este transcribirá en un elemento del html que nosotros designaremos.
-Es un pequeno tweet.
-Luego de publicar nuestro mensaje este se limpiará automáticamente para luego volver a escribir normalmente.
-Al publicar nuestro mensaje este se acumulará cuantas veces publiquemos, pero será ocupando el lugar del anterior. 
-
-v0.0.2
-
-En este ejemplo
-1. No ingresar texto vacío (deshabilitar el botón de enviar).
-2. Contar la cantidad de caracteres de forma regresiva.
-
-Versión 0.0.3:
-1. Si pasa los 140 caracteres, deshabilitar el botón.
-2. Si pasa los 120 caracteres, mostrar el contador con OTRO color.
-3. Si pasa los 130 caracteres, mostrar el contador con OTRO color.
-4. si pasa los 140 caracteres, mostrar el contador en negativo.
+Versión 0.0.4:
+1. Al presionar enter ("/n") que crezca el textarea de acuerdo al tamaño del texto.
--- js/app.js
+++ js/app.js
@@ -6,8 +6,7 @@ window.addEventListener("load", function() {
 		var text=document.getElementById("text").value;
 		newtexto(text);
 		document.getElementById("text").value="";
-		btn.disabled=true;
-		
+		btn.disabled=true;		
 	});
 	function newtexto(text){
 		var elemento=document.createElement("div");
@@ -29,15 +28,18 @@ window.addEventListener("load", function() {
 		document.getElementById("contador").innerHTML=limite-longitud;
 		if (longitud>120 && longitud<130) {
 			document.getElementById("contador").classList.add("caracter120");
-		}
-		else if (longitud>130 && longitud<140) {
+		}else if (longitud>130 && longitud<140) {
 			document.getElementById("contador").classList.add("caracter130");
-		}
-		else if(longitud >= 140){
+		}else if(longitud > 140){
 			btn.disabled=true;
-			document.getElementById("contador").classList.add("caracter140");
-		}
+		}       
 	}
-
-
+	text.addEventListener('keydown', autosize); 
+    function autosize(){
+      var el = this;
+      setTimeout(function(){
+        el.style.cssText = 'height:auto; padding:0';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+      },0);
+    } 
 });
