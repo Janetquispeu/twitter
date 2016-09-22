e6ef457fa6d6a31a5d5396e314efc06b69b211e2
smiling girl.. sonrie   siempre  en la   vida <smiling girl.. sonrie   siempre  en la   vida>
2016-09-22 01:16:58 -0500

    Estilos al twitter

--- js/app.js
+++ js/app.js
@@ -1,51 +1,58 @@
 window.addEventListener("load", function() {
 	var btn=document.getElementById("btn");	
 	btn.disabled=true;
+
 	btn.addEventListener("click",function(e){
 		e.preventDefault();
-		var text=document.getElementById("text").value;
-		newtexto(text);
-		document.getElementById("text").value="";
+		var text = document.getElementById("text").value;
+		publicarTexto(text);
+
+		document.getElementById("text").value = "";
 		document.getElementById("contador").innerHTML=140;
 		btn.disabled=true;		
 	});
-	function newtexto(text){
-		var elemento=document.createElement("div");
-		var conten=document.getElementById("conten");
-		var hora = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
-		elemento.innerText=text + " "+ hora;
-
-		if(!conten.childNodes[0]){
-			conten.appendChild(elemento);
-		}else{
-			conten.insertBefore(elemento,conten.childNodes[0]);
-		}
-	}
+	
 	text.addEventListener("keydown",function(){
 		contador(text);
 	});
-	function contador(text){
-		btn.disabled=false;
-		var limite =140;
-		var longitud=document.getElementById("text").value.length;
-		document.getElementById("contador").innerHTML=limite-longitud;
-		if (longitud>120 && longitud<130) {
-			document.getElementById("contador").classList.add("caracter120");
-		}else if (longitud>130 && longitud<140) {
-			document.getElementById("contador").classList.add("caracter130");
-		}else if(longitud > 140){
-			btn.disabled=true;
-		document.getElementById("contador").classList.add("caracter140");
-		}    
-		else if(longitud==0){
-			btn.disabled=true;
-		}   
-	}
+	
 	text.addEventListener('keydown', autosize); 
-    function autosize(){
-      var el = this;
-        el.style.cssText = 'height:auto; padding:0';
-        el.style.cssText = 'height:' + el.scrollHeight + 'px';
-      
-    }
+    
 });
+function publicarTexto(text){
+	var elemento=document.createElement("div");
+	var conten=document.getElementById("conten");
+	var hora = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
+	elemento.innerText=text + hora;
+
+	if(!conten.childNodes[0]){
+		conten.appendChild(elemento);
+	}else{
+		conten.insertBefore(elemento,conten.childNodes[0]);
+	}
+}
+function contador(text){
+	btn.disabled=false;
+	var limite =140;
+	var longitud=document.getElementById("text").value.length;
+	document.getElementById("contador").innerHTML=limite-longitud;
+
+	if (longitud>120 && longitud<130) {
+		document.getElementById("contador").classList.add("caracter120");
+	}else if (longitud>130 && longitud<140) {
+		document.getElementById("contador").classList.add("caracter130");
+	}else if(longitud > 140){
+		btn.disabled=true;
+	document.getElementById("contador").classList.add("caracter140");
+	}    
+	else if(longitud==0){
+		btn.disabled=true;
+	}   
+}
+
+function autosize(){
+  var el = this;
+    el.style.cssText = 'height:auto; padding:0';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  
+}
